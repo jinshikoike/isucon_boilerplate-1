587faae8154c714a2d541792acf5c482b6e39c0d
tanishi <tanishi503@gmail.com>
2017-10-15 12:31:33 +0900

    Add make initialize line in go file

--- add_logger.sh
+++ add_logger.sh
@@ -1,1 +1,2 @@
-find . -name \*.go | xargs sed -r "s/(^func.*?$)/\1\n\tdefer MeasureEnd(MeasureStart(), file)/" -i
+find . -name \*.go | xargs sed -r "s/(^func main.*?$)/var file = Initialize()\n\1/" -i
+find . -name \*.go | xargs sed -r "s/(^func.*?$)/\1\n\tdefer MeasureEnd(MeasureStart(""))/" -i
--- remove_logger.sh
+++ remove_logger.sh
@@ -1,1 +1,2 @@
 find . -name \*.go | xargs sed -r "/(^\tdefer.*$)/d"
+find . -name \*.go | xargs sed -r "/(^var file =.*$)/d"
